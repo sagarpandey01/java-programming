93ddcef674c46eda8e231d96aaa6b9ffa8ac7a86
sagar pandey <sagarbaba0101@gmail.com>
2026-02-12 14:54:43 +0530

    arraycode

---
+++ arry.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class arry {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter size");
+        int size = sc.nextInt();
+        int number[] = new int[size];
+        for(int i=0;i<size;i++){
+            number[i]=sc.nextInt();
+        }
+        int x = sc.nextInt();
+        for(int i=0;i<number.length;i++){
+            if(number[i]==x){
+                System.out.println("x found at index" + i);
+            }
+        }
+    }
+    
+}
